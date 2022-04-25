ba9d366f183f7cf71f5d348f7af7794bc225b906
sethsamrat <sethsamrat27@gmail.com>
2022-04-25 19:37:43 +0530

    Initial commit

---
+++ db.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+var mongoURL = 'mongodb+srv://sethsamrat:27092410@cluster0.mzjcy.mongodb.net/Buttercrust'
+
+mongoose.connect(mongoURL , {useUnifiedTopology:true , useNewUrlParser:true})
+
+var db = mongoose.connection
+
+db.on('connected' , ()=>{
+    console.log('Mongo DB Connection Successfull');
+})
+
+db.on('error' , ()=>{
+    console.log(`Mongo DB Connection failed`);
+})
+
+module.exports =mongoose
